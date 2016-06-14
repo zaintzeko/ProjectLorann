c6871b0060a2aaee64a2a5d5492d134f6928b7fe
wayzen <wayzen@10.133.128.153>
2016-06-14 16:02:55 +0200

    Add JUnit test

---
+++ PRoject/model/src/main/java/model/lorannWorld/LorannWorldTest.java
@@ -0,0 +1,54 @@
+package model.lorannWorld;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LorannWorldTest {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetElement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMotionlessElements() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMotionElements() {
+		fail("Not yet implemented");
+	}
+
+}
