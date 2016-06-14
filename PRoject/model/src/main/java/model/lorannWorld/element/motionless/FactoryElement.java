a71066702ef2e205555914ccaf6cc223607035d8
zaintzeko <q.drillon@hotmail.fr>
2016-06-14 16:33:34 +0200

    add FActoryElement

--- PRoject/model/src/main/java/model/lorannWorld/element/motionless/FactoryElement.java
+++ PRoject/model/src/main/java/model/lorannWorld/element/motionless/FactoryElement.java
@@ -3,11 +3,11 @@ package model.lorannWorld.element.motionless;
 import model.lorannWorld.element.Permeability;
 import model.lorannWorld.element.motion.Lorann;
 
-public class FactoryElement {
-private static  Lorann LORANN = new Lorann();
-private static MotionlessElement HORIZONTALBONE = new MotionlessElement(null, Permeability.BLOCKING);
-private static MotionlessElement VERTICALBONE = new MotionlessElement(null, Permeability.BLOCKING);
-private static MotionlessElement KNEEPCAP = new MotionlessElement(null, Permeability.BLOCKING);
-private static MotionlessElement TREASURE = new MotionlessElement(null, Permeability.RAMASSABLE);
-private static MotionlessElement KEY = new MotionlessElement(null, Permeability.RAMASSABLE);
+public abstract class  FactoryElement {
+public static  Lorann LORANN = new Lorann();
+public static MotionlessElement HORIZONTALBONE = new MotionlessElement(null, Permeability.BLOCKING);
+public static MotionlessElement VERTICALBONE = new MotionlessElement(null, Permeability.BLOCKING);
+public static MotionlessElement KNEEPCAP = new MotionlessElement(null, Permeability.BLOCKING);
+public static MotionlessElement TREASURE = new MotionlessElement(null, Permeability.RAMASSABLE);
+public static MotionlessElement KEY = new MotionlessElement(null, Permeability.RAMASSABLE);
 }
