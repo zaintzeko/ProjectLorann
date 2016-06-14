68d3910942bb24ebd65fac8fa097b35f0b3819a4
wayzen <wayzen@10.133.128.153>
2016-06-14 10:34:33 +0200

    Cordked code of Map and MotionlessElement

---
+++ PRoject/model/src/main/java/model/LorannWorld.java
@@ -0,0 +1,25 @@
+package model;
+
+public class LorannWorld {
+	private int width;
+	private int height;
+	
+	public LorannWorld(int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+	}
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	public Element getElement( int x, int y){
+		
+	}
+	public void addElement(Element element, int x, int y){ 
+		
+	}
+}
