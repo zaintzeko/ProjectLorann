77a0ce23c689847cef75bf1e119109cc19c7c479
wayzen <wayzen@10.133.128.153>
2016-06-17 14:48:48 +0200

    fake

--- PRoject/model/src/main/java/model/lorannWorld/element/motion/Lorann.java
+++ PRoject/model/src/main/java/model/lorannWorld/element/motion/Lorann.java
@@ -1,23 +1,27 @@
 package model.lorannWorld.element.motion;
 
+import java.awt.event.KeyEvent;
+
 import contract.ILorann;
 import contract.ILorannWorld;
 import contract.ISprite;
 import model.lorannWorld.element.Permeability;
 
 public class Lorann extends MotionElement implements ILorann{
+	private int keyCode;
 	public Lorann(final ISprite sprite, final Permeability permeability, final char symbole, final ILorannWorld lorannWorld) {
 		super(sprite, permeability, symbole, lorannWorld);
-		// TODO Auto-generated constructor stub
+
 	}
 
-	/* (non-Javadoc)
-	 * @see model.IBehaviorMonsterSkeleton#animate()
-	 */
-	/* (non-Javadoc)
-	 * @see model.lorannWorld.element.motion.ILorann#animate()
-	 */
+	public void setKeyCode(int keyCode){
+		this.keyCode = keyCode;
+	}
+
 	public void animate() {
-
+		if(keyCode == KeyEvent.VK_UP || keyCode == KeyEvent.VK_DOWN || keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_LEFT){
+			
+		}
 	}
+
 }
