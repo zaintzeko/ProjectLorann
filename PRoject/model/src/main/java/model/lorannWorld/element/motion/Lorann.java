250ba85da4c3a6bb7fde3182da41e73ac734cb76
Alexandre <Alexandre@CrYo-Alex>
2016-06-17 14:49:28 +0200

    fake

--- PRoject/model/src/main/java/model/lorannWorld/element/motion/Lorann.java
+++ PRoject/model/src/main/java/model/lorannWorld/element/motion/Lorann.java
@@ -1,11 +1,12 @@
 package model.lorannWorld.element.motion;
 
+import contract.ILorannWorld;
 import contract.ISprite;
 import model.lorannWorld.element.Permeability;
 
 public class Lorann extends MotionElement{
-	public Lorann(final ISprite sprite, final Permeability permeability, final char symbole) {
-		super(sprite, permeability, symbole);
+	public Lorann(final ISprite sprite, final Permeability permeability, final char symbole, final ILorannWorld lorannWorld) {
+		super(sprite, permeability, symbole, lorannWorld);
 		// TODO Auto-generated constructor stub
 	}
 
