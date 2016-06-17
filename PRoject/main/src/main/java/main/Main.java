8cf1df4fed76339cf6a060641929a19f7823faf3
dorianBataille <dorba@DESKTOP-386UJD0.mshome.net>
2016-06-17 19:58:55 +0200

    save the modification in level map

--- PRoject/main/src/main/java/main/Main.java
+++ PRoject/main/src/main/java/main/Main.java
@@ -3,7 +3,6 @@ package main;
 
 import controller.Controller;
 import model.Model;
-import saveInBDDByMapText.SaveMap;
 import view.View;
 
 /**
@@ -22,8 +21,8 @@ public abstract class Main {
 	 */
 	public static void main(final  String[] args) throws Exception {
 
-		SaveMap.saveAllMap();
-		final int LEVEL = 1;
+		//SaveMap.saveAllMap();
+		final int LEVEL = 3;
 
 		final Model model = new Model(LEVEL);
 		final View view = new View(model);
