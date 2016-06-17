b550b56ebd3dfd4b59393d4716591360c14607ab
Jean-Aymeric Diet <jadiet@cesi.fr>
2016-06-17 16:28:15 +0200

    Ajoût d'un System.out dans le main pour faire un test de commit et essayer d'identifier le problème github

--- PRoject/main/src/main/java/main/Main.java
+++ PRoject/main/src/main/java/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-
 import controller.Controller;
 import model.Model;
 import view.View;
@@ -13,22 +12,20 @@ import view.View;
 public abstract class Main {
 
 	/**
-	 * The main method    .
+	 * The main method .
 	 *
 	 * @param args
 	 *          the arguments
 	 * @throws Exception
 	 */
-	public static void main(final  String[] args) throws Exception {
+	public static void main(final String[] args) throws Exception {
 
-
-
-		/*try {
-			new LoadTextFile().loadFile("level/lvl3.txt");
-		} catch (final IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		/*
+		 * try { new LoadTextFile().loadFile("level/lvl3.txt"); } catch (final IOException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+		// JADIET : Ceci est un test fait depuis eclipse pour voir quel est le souci avec GitHub
+		System.out.println("Test JADT depuis eclipse.");
 
 		final Model model = new Model();
 		final View view = new View(model);
