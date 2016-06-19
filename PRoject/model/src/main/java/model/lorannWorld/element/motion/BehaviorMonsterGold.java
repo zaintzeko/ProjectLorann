dbd7a07c64acbe0b6abaa2d56f474f7b38a0663a
zaintzeko <q.drillon@hotmail.fr>
2016-06-19 16:20:26 +0200

    why commit?

--- PRoject/model/src/main/java/model/lorannWorld/element/motion/BehaviorMonsterGold.java
+++ PRoject/model/src/main/java/model/lorannWorld/element/motion/BehaviorMonsterGold.java
@@ -1,28 +1,54 @@
 package model.lorannWorld.element.motion;
 
+
+
 import contract.ILorannWorld;
 import contract.IMotionElement;
 import contract.IStrategy;
 
 public class BehaviorMonsterGold extends Monster implements IStrategy{
 
+
+
 	/* (non-Javadoc)
 	 * @see model.IBehaviorMonsterSkeleton#animate()
 	 */
-	public void animate(IMotionElement motionElement,ILorannWorld lorannWorld) {
+	public void animate(final IMotionElement motionElement,final ILorannWorld lorannWorld) {
 		boolean a = false;
-		
+
 		if(lorannWorld.getLorann().getY()<motionElement.getY()){
 			a = motionElement.moveUp();
+
 		}
-		if (lorannWorld.getLorann().getX()<motionElement.getX()){
-			a = motionElement.moveLeft();
+		else {
+			if (lorannWorld.getLorann().getX()<motionElement.getX()){
+				a = motionElement.moveLeft();
+			}
 		}
-		if (lorannWorld.getLorann().getX()>motionElement.getX()){
-			a = motionElement.moveRight();
+		else{
+			if (lorannWorld.getLorann().getX()>motionElement.getX()){
+				a = motionElement.moveRight();
+			}
 		}
 		if (lorannWorld.getLorann().getY()>motionElement.getY()){
 			a = motionElement.moveDown();
+
+		}
+
+
+
+		if ((lorannWorld.getLorann().getY()<motionElement.getY()) && (lorannWorld.getLorann().getX()<motionElement.getX()) && !a){
+			a = motionElement.moveLeftUp();
+		}
+		if ((lorannWorld.getLorann().getY()<motionElement.getY()) && (lorannWorld.getLorann().getX()>motionElement.getX()) && !a){
+			a = motionElement.moveRightUp();
+		}
+		if ((lorannWorld.getLorann().getY()>motionElement.getY()) && (lorannWorld.getLorann().getX()<motionElement.getX()) && !a){
+			a = motionElement.moveLeftDown();
+		}
+		if ((lorannWorld.getLorann().getY()>motionElement.getY()) && (lorannWorld.getLorann().getX()>motionElement.getX()) && !a){
+			a = motionElement.moveRightDown();
 		}
 	}
+
 }
