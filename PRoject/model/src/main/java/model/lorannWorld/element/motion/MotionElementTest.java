6d1e8f8f4259cf88328975ea343ea18cb122b1a7
wayzen <wayzen@10.133.128.153>
2016-06-20 11:19:00 +0200

    Test: Correction of bug Class: MotionElement

--- PRoject/model/src/main/java/model/lorannWorld/element/motion/MotionElementTest.java
+++ PRoject/model/src/main/java/model/lorannWorld/element/motion/MotionElementTest.java
@@ -2,14 +2,125 @@ package model.lorannWorld.element.motion;
 
 import static org.junit.Assert.*;
 
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Observable;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class MotionElementTest {
+import contract.ILorann;
+import contract.ILorannWorld;
+import contract.IMobile;
+import contract.IMotionlessElement;
+import contract.ISprite;
 
+public class MotionElementTest {
+	// Seems like Decorator ? 
+	private MotionElement testMotion = new Lorann(new ISprite() {
+
+		public void changecurentImage(int x) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void changeToNextImage() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public Image getImage() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public int getCurrentStep() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}, 'L', new ILorannWorld(){
+
+		public void addElement(IMobile motionElement, int x, int y) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void addElement(IMotionlessElement motionlessElement, int x, int y) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void addScore(int x) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public IMotionlessElement getElement(int x, int y) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMotionlessElement[][] getElements() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public int getHeight() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public ILorann getLorann() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMobile getMobileX(int x) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public ArrayList<IMobile> getMotionElements() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public Observable getObservable() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public int getScore() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public int getWidth() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		public void play() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public boolean removeMobile(int x, int y) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public void setWin(int win) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	});
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -28,47 +139,51 @@ public class MotionElementTest {
 
 	@Test
 	public void testExecuteMoveIfPossible() {
-		fail("Not yet implemented");
+		assertEquals(true, this.testMotion.executeMoveIfPossible(0, 0));
 	}
-
-	@Test
-	public void testGetDirection() {
-		fail("Not yet implemented");
-	}
-
 	@Test
 	public void testGetLorannWorld() {
-		fail("Not yet implemented");
+		assertNotNull(this.testMotion.getLorannWorld());
 	}
 
 	@Test
 	public void testGetSaveX() {
-		fail("Not yet implemented");
+		int expected = 1;
+		this.testMotion.setSaveX(1);
+		assertEquals(1,this.testMotion.getSaveX());
 	}
 
 	@Test
 	public void testGetSaveY() {
-		fail("Not yet implemented");
+		int expected = 1;
+		this.testMotion.setSaveY(1);
+		assertEquals(1,this.testMotion.getSaveY());
 	}
 
 	@Test
 	public void testGetX() {
-		fail("Not yet implemented");
+		int expected = 1;
+		this.testMotion.setX(1);
+		assertEquals(1,this.testMotion.getX());
 	}
 
 	@Test
 	public void testGetY() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testKillElement() {
-		fail("Not yet implemented");
+		int expected = 1;
+		this.testMotion.setY(1);
+		assertEquals(1,this.testMotion.getY());
 	}
 
 	@Test
 	public void testSavePosition() {
-		fail("Not yet implemented");
+		this.testMotion.setX(1);
+		this.testMotion.setY(1);
+		this.testMotion.setSaveX(2);
+		this.testMotion.setSaveY(2);
+		this.testMotion.savePosition();
+		int expected = 1;
+		assertEquals(expected, this.testMotion.getSaveX());
+		assertEquals(expected, this.testMotion.getSaveY());
 	}
 
 }
