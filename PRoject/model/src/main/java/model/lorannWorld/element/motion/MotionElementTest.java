3426be39066d53c875e4a54239527c9e77625415
SylvainMoretti <wayzen@10.133.128.153>
2016-06-20 10:42:51 +0200

    Test : Modification of Element Class : MotionElement

---
+++ PRoject/model/src/main/java/model/lorannWorld/element/motion/MotionElementTest.java
@@ -0,0 +1,74 @@
+package model.lorannWorld.element.motion;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MotionElementTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testExecuteMoveIfPossible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDirection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLorannWorld() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSaveX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSaveY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKillElement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSavePosition() {
+		fail("Not yet implemented");
+	}
+
+}
