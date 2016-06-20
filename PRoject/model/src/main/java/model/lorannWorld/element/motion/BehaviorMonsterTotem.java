286ae1b24b16cc63f7ebbfc49deb8e75a45f095c
Alexandre <Alexandre@CrYo-Alex>
2016-06-20 13:11:25 +0200

    Ending for totem AI

--- PRoject/model/src/main/java/model/lorannWorld/element/motion/BehaviorMonsterTotem.java
+++ PRoject/model/src/main/java/model/lorannWorld/element/motion/BehaviorMonsterTotem.java
@@ -14,35 +14,49 @@ public class BehaviorMonsterTotem extends Monster implements IStrategy{
 		motionElement.savePosition();
 		Random rand = new Random();
 		int Nomber = rand.nextInt(8);
-		
+
 		
 		switch(Nomber){
 		case 0:
 		
-
+			if(lorannWorld.getLorann().getY()<motionElement.getY()){
+				motionElement.setY(motionElement.getY()-1);
+			}
 			break;
 		case 1:
-	
+			if(lorannWorld.getLorann().getY()>motionElement.getY()){
+				motionElement.setY(motionElement.getY()+1);
+			}
 			break;
 		case 2:
-
+			if (lorannWorld.getLorann().getX()>motionElement.getX()){
+				motionElement.setX(motionElement.getX()+1);
+			}
 			break;
 		case 3:
-		
+			if (lorannWorld.getLorann().getX()<motionElement.getX()){
+				
+			motionElement.setX(motionElement.getX()-1);
+		}
 			break;
 		case 4:
-	
+				motionElement.setY(motionElement.getY()-1);
+				motionElement.setX(motionElement.getX()-1);
 			break;
 		case 5:
-		
+				motionElement.setY(motionElement.getY()-1);
+				motionElement.setX(motionElement.getX()+1);
 			
 			break;
 		case 6:
-	
+				motionElement.setY(motionElement.getY()+1);
+				motionElement.setX(motionElement.getX()-1);
 			break;
 		case 7:
-		
+				motionElement.setY(motionElement.getY()+1);
+				motionElement.setX(motionElement.getX()+1);
 			default:
 		}
+		motionElement.executeMoveIfPossible(motionElement.getX(), motionElement.getY());
 	}
 }
